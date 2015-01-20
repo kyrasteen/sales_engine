d9e1e30da9d039f605b8a917a8a3a57118117fce
Kyra Weber <kyra.m.weber@gmail.com>
2015-01-20 16:13:37 -0700

    Implemented #load_file in #transactions

---
+++ lib/transactions_repo.rb
@@ -0,0 +1,28 @@
+require 'csv'
+
+class TransactionsRepo
+
+  attr_reader :data
+
+  def initialize
+    @data = []
+  end
+
+  def load_file(path)
+    rows = CSV.open(path, headers:true, header_converters: :symbol)
+    @data = rows.select do |row|
+        row
+      end
+      data
+  end
+
+  def invoice_id(row_id)
+    #travel to row of row_id passed in
+    line = data.select do |row|
+      row[:id] == row_id
+    end
+    #then find its invoice_id
+    line[:invoice_id]
+  end
+
+end
---
+++ test/transactions_repository_test.rb
@@ -0,0 +1,26 @@
+require_relative '../lib/transactions_repo'
+require 'minitest/autorun'
+require 'minitest/pride'
+
+class TransactionsRepoTest < Minitest::Test
+
+  attr_reader :trans_repo
+
+  def setup
+    @trans_repo = TransactionsRepo.new
+  end
+
+  def test_it_exists
+    assert trans_repo = TransactionsRepo.new
+  end
+
+  def test_file_has_19_lines
+    assert_equal 19,trans_repo.load_file('./fixtures/transactions_test_data.csv').length
+  end
+
+  def test_fourth_record_has_invoice_id
+    skip
+    assert_equal 5,trans_repo.invoice_id(4)
+  end
+
+end
