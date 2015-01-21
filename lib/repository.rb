dd37d32f67a82c21152964be17618ff41186e542
Lori Culberson <bigskybandit@sbcglobal.net>
2015-01-20 22:41:03 -0700

    trans repo with find_by_id test failure

--- lib/repository.rb
+++ lib/repository.rb
@@ -1,7 +1,21 @@
 class Repository
 
   def all
-    [5,2]
+   
   end
 
 end
+
+
+
+ def find_by_id(id)
+    find_by_attribute(:id, id)
+  end
+
+  def find_by_created_at(created_at)
+    find_by_attribute(:created_at, created_at)
+  end
+
+  def find_by_updated_at(updated_at)
+    find_by_attribute(:updated_at, updated_at)
+  end
--- test/transactions_repository_test.rb
+++ test/transactions_repository_test.rb
@@ -1,30 +1,34 @@
-require_relative '../lib/transactions_repo'
 require 'minitest/autorun'
-require 'minitest/pride'
+# require 'minitest/pride'
+require '../lib/transactions_repo'
+require 'pry'
 
 class TransactionsRepoTest < Minitest::Test
-
   attr_reader :trans_repo
 
   def setup
-    @trans_repo = TransactionsRepo.new
+    @trans_repo = TransactionsRepo.new('../fixtures/transactions_test_data.csv')
   end
 
-  def test_it_exists
-    assert trans_repo = TransactionsRepo.new
+  def test_array_is_created_when_file_loads
+    assert trans_repo.load_data.is_a?(Array) 
   end
 
-  def test_file_has_19_lines
-    assert_equal 19,trans_repo.load_file('./fixtures/transactions_test_data.csv').length
+  def test_file_has_19_records
+    trans_repo.load_data
+    assert_equal 19, trans_repo.all.count
   end
 
-  def test_fourth_record_has_invoice_id
-    skip
-    assert_equal 5,trans_repo.invoice_id(4)
+  def test_it_can_find_a_random_record
+    trans_repo.load_data
+    assert trans_repo.random
   end
 
-  def test_it_finds_all
-    
+  def test_can_find_an_id_number
+    trans_repo.load_data
+    assert_equal 5,trans_repo.find_by_id(4)
   end
+
+
 
 end
