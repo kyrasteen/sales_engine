e5d29156623b06e0f6fe810811b0ad02b230b390
Kyra Weber <kyra.m.weber@gmail.com>
2015-01-21 17:52:28 -0700

    Implemented common methods in merchant

--- lib/merchants.rb
+++ lib/merchants.rb
@@ -1,11 +1,11 @@
 class Merchants
 
-  attr_reader :id, :name
+  attr_reader :id, :name, :created_at
 
   def initialize(line, my_parent)
-    @line = line
     @id = line[:id].to_i
     @name = line[:name]
+    @created_at = line[:created_at]
   end
 
 end
--- lib/merchants_repo.rb
+++ lib/merchants_repo.rb
@@ -3,16 +3,63 @@ require_relative 'merchants_parser'
 
 class MerchantsRepo
 
-  attr_reader :invoices, :data
+  attr_reader :invoices, :data, :merchants_parser
 
-  def initialize
-    merchants = MerchantsParser.new(filename)
-    @data = merchants.parse
+  def initialize(filename)
+    @merchants_parser = MerchantsParser.new(filename)
+    @data = merchants_parser.parse
   end
 
-  def find_invoices_by_merchant_id(id_number)
-    @invoices =
+  def all
+    data
+  end
 
+  def random
+    record = rand(0..data.length)
+    data[record]
+  end
+
+  def find_by_id(id)
+    find_by_attribute(:id, id)
+  end
+
+  def find_all_by_id(id)
+    find_all_by_attribute(:id, id)
+  end
+
+  def find_by_name(name)
+    find_by_attribute(:name, name)
+  end
+
+  def find_all_by_name(name)
+    find_all_by_attribute(:name, name)
+  end
+
+  def find_by_created_at(time)
+    find_by_attribute(:created_at, time)
+  end
+
+  def find_all_by_created_at(time)
+    find_all_by_attribute(:created_at, time)
+  end
+
+  def find_by_attribute(attribute,criteria)
+    data.each_with_index do |row, index|
+      if row.send(attribute) == criteria
+        return data[index]
+      end
+    end
+  end
+
+  def find_all_by_attribute(attribute, criteria)
+    all_found = []
+    data.each_with_index do |row, index|
+      # require 'pry'; binding.pry
+      if row.send(attribute) == criteria
+        all_found << data[index]
+      end
+    end
+    all_found
   end
 
 
