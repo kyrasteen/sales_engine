c917ed63ea4d2a2cf3f65ccd1fc5414f54721f5d
Kyra Weber <kyra.m.weber@gmail.com>
2015-01-21 22:28:23 -0700

    Implemented basics of #customers, parser and repo

--- lib/customers.rb
+++ lib/customers.rb
@@ -1,2 +1,12 @@
 class Customers
+  attr_reader :id, :first_name, :last_name, :created_at, :updated_at
+
+  def initialize(data, my_parent)
+    @id = data[:id].to_i
+    @first_name = data[:first_name]
+    @last_name = data[:last_name]
+    @created_at = data[:created_at]
+    @updated_at = data[:updated_at]
+  end
+
 end
---
+++ test/customers_repository_test.rb
@@ -0,0 +1,41 @@
+require 'minitest/autorun'
+require 'minitest/pride'
+require_relative '../lib/customers_repo'
+require 'pry'
+
+class CustomersRepoTest < Minitest::Test
+
+  def setup
+    filename = ('./test/support/customers_test_data.csv')
+    @customers = CustomersRepo.new(filename)
+  end
+
+  def test_it_finds_all
+    assert_equal 19, @customers.all.length
+  end
+
+  def test_it_can_find_by_id
+    assert_equal "1", @customers.find_by_id(1).id.to_s
+  end
+
+  def test_it_can_find_all_by_id
+    assert_equal 2, @customers.find_all_by_id(19).count
+  end
+
+  def test_it_can_find_by_created_at
+    assert_equal "2012-03-27 14:54:10 UTC", @customers.find_by_created_at('2012-03-27 14:54:10 UTC').created_at
+  end
+
+  def test_it_can_find_all_by_created_at
+    assert_equal 6, @customers.find_all_by_created_at('2012-03-27 14:54:10 UTC').count
+  end
+
+  def test_it_can_find_by_updated_at
+    assert_equal "2012-03-27 14:54:10 UTC", @customers.find_by_updated_at('2012-03-27 14:54:10 UTC').updated_at
+  end
+
+  def test_it_can_find_all_by_updated_at
+    assert_equal 6, @customers.find_all_by_updated_at('2012-03-27 14:54:10 UTC').count
+  end
+
+end
