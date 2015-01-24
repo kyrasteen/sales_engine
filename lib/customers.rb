f7ea1eca3cd91a6f171207fb9036a8fc5129f9f3
Kyra Weber <kyra.m.weber@gmail.com>
2015-01-23 18:09:34 -0700

    Implemented relationship methods in #customers

--- lib/customers.rb
+++ lib/customers.rb
@@ -1,16 +1,17 @@
 class Customers
-  attr_reader :id, :first_name, :last_name, :created_at, :updated_at, :mr_self
+  attr_reader :id, :first_name, :last_name, :created_at, :updated_at, :cr_self
 
-  def initialize(data, mr_self)
+  def initialize(data, cr_self)
     @id = data[:id].to_i
     @first_name = data[:first_name]
     @last_name = data[:last_name]
     @created_at = data[:created_at]
     @updated_at = data[:updated_at]
-    @mr_self = mr_self
+    @cr_self = cr_self
   end
 
   def invoices
+    cr_self.find_all_by_id(id)
   end
 
 end
